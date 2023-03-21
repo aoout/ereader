6265d2885890593862e2b6fa3e5a01580ed3a692
aoout <wuz66280@gmail.com>
2023-03-21 20:37:00 +0800

    separate files

---
+++ epubparser.py
@@ -0,0 +1,62 @@
+import os
+import shutil
+import tempfile
+import zipfile
+from functools import cached_property
+from pathlib import Path
+from typing import List
+
+import xmltodict
+
+
+class EpubParser:
+    def __init__(self, filename: str) -> None:
+        """
+        Initialize EpubParser
+        """
+        self.filename = filename
+        self.tempdir = Path(tempfile.TemporaryDirectory().name)
+        self.current_page_index = 0
+        self.opf_file = self.tempdir / 'content.opf'
+
+    @cached_property
+    def pages_path(self) -> List[str]:
+        if self.tempdir.exists():
+            shutil.rmtree(self.tempdir)
+        os.makedirs(self.tempdir)
+        with zipfile.ZipFile(self.filename, 'r') as zip_ref:
+            zip_ref.extractall(self.tempdir)
+
+        with open(self.opf_file, 'r', encoding='utf-8') as f:
+            opf_content = f.read()
+
+        opf_dict = xmltodict.parse(opf_content)
+        manifest = opf_dict['package']['manifest']
+        pages = [self.tempdir / item['@href'] for item in manifest['item'] if
+                           item['@media-type'] == 'application/xhtml+xml']
+        pages.sort()
+        return pages
+
+
+    def get_next_page_content(self) -> str:
+        """
+        Get next page content
+        """
+        if self._has_next_page():
+            self.current_page_index += 1
+            return self._get_page_content(self.current_page_index)
+        else:
+            return None
+
+    def _get_page_content(self, index: int) -> str:
+        """
+        Get page content by index
+        """
+        with open(self.pages_path[index], 'r', encoding='utf-8') as f:
+            page_content = f.read()
+        return page_content
+    def _has_next_page(self) -> bool:
+        """
+        Check if there is next page
+        """
+        return self.current_page_index < len(self.pages_path) - 1
