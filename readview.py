76a044e4569ca28db762ab975d5aaad883a99f2f
aoout <wuz66280@gmail.com>
2023-03-23 22:34:12 +0800

    fix page switch bug

--- readview.py
+++ readview.py
@@ -17,6 +17,7 @@ class ReadView(WebView):
         super().__init__(parent)
         settings = QWebEngineSettings.globalSettings()
         settings.setFontFamily(QWebEngineSettings.StandardFont,"LXGW WenKai")
+        settings.setFontSize(QWebEngineSettings.DefaultFontSize,24)
         self.epub_parser = None
 
     def setHtml(self, html: str, baseUrl: QtCore.QUrl = QtCore.QUrl("")) -> None:
@@ -48,7 +49,6 @@ class ReadView(WebView):
         logging.info(f"Loaded HTML file: {self.epub_parser.pages_path[0]}")
 
     def wheelEvent(self, e: QtGui.QWheelEvent) -> None:
-        print(self.loading)
         if not self.loading:
             bias = e.angleDelta().y()
             if bias > 0:
@@ -62,8 +62,9 @@ class ReadView(WebView):
         Load the next page
         """
         if self.epub_parser.current_page_index != len(self.epub_parser.pages_path)-1:
-            self.setHtml(self.epub_parser.get_page_content(self.epub_parser.current_page_index))
             self.epub_parser.current_page_index += 1
+            self.setHtml(self.epub_parser.get_page_content(self.epub_parser.current_page_index))
+
             logging.info(f"Loaded HTML file: {self.epub_parser.pages_path[self.epub_parser.current_page_index]}")
         else:
             logging.info("No more HTML files to load")
@@ -74,8 +75,9 @@ class ReadView(WebView):
         """
 
         if self.epub_parser.current_page_index != 0:
-            self.setHtml(self.epub_parser.get_page_content(self.epub_parser.current_page_index-1))
             self.epub_parser.current_page_index -= 1
+            self.setHtml(self.epub_parser.get_page_content(self.epub_parser.current_page_index))
+
             logging.info(f"Loaded HTML file: {self.epub_parser.pages_path[self.epub_parser.current_page_index]}")
             self.runALF(lambda :self.page().runJavaScript("window.scrollTo(0, document.body.scrollHeight);"))
         else:
