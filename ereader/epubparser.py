64347c72d06f1b80b3a0736f09866059dc26cee0
aoout <wuz66280@gmail.com>
2023-03-26 01:30:44 +0800

    parse epub's toc

--- ereader/epubparser.py
+++ ereader/epubparser.py
@@ -7,7 +7,6 @@ from typing import List, Tuple
 
 import xmltodict
 
-
 class EpubParser:
     def __init__(self, filename: str) -> None:
         """
@@ -21,6 +20,7 @@ class EpubParser:
         self.tempdir = self.opf_file.parent
 
         self.pages_path ,self.css_path = self.parse()
+        self.toc = self.parse_toc()
 
     def extract(self):
         if self.tempdir.exists():
@@ -30,8 +30,6 @@ class EpubParser:
             zip_ref.extractall(self.tempdir)
 
     def parse(self) -> Tuple[List[Path],List[Path]]:
-
-
         with open(self.opf_file, 'r', encoding='utf-8') as f:
             opf_content = f.read()
 
@@ -41,6 +39,37 @@ class EpubParser:
                            item['@media-type'] == 'application/xhtml+xml']
         css = [self.tempdir /item['@href'] for item in manifest['item'] if item['@media-type'] == 'text/css']
         return pages,css
+
+    def parse_toc(self):
+        toc_file = next(Path(self.tempdir).rglob('toc.ncx'), None)
+        print(toc_file)
+        with open(toc_file, 'r', encoding='utf-8') as f:
+            toc_content = f.read()
+
+        toc_dict = xmltodict.parse(toc_content)
+        nav_map = toc_dict['ncx']['navMap']
+        toc = []
+        for item in nav_map['navPoint']:
+            toc_item = {'text': item['navLabel']['text'], 'url': self.tempdir / item['content']['@src']}
+            if 'navPoint' in item:
+                toc_item['subitems'] = []
+                for subitem in item['navPoint']:
+                    subitem = {'text': subitem['navLabel']['text'], 'url': self.tempdir / subitem['content']['@src']}
+                    toc_item['subitems'].append(subitem)
+            toc.append(toc_item)
+        return toc
+
+    def print_toc(self):
+        def print_toc_item(item, level=0):
+            print('  ' * level + item['text'])
+            if 'subitems' in item:
+                for subitem in item['subitems']:
+                    print_toc_item(subitem, level + 1)
+
+        for item in self.toc:
+            print_toc_item(item)
+
+
 
     def currentPagePath(self) -> Path:
         return self.pages_path[self.current_page_index]
